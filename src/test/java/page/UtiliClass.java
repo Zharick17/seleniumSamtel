b57407e3d1daa625ed2e455c3dd4f68e348f9840
Zharick Daniela Barreto Ramirez <Zharick.Barreto@samtel.local>
2024-05-22 14:11:57 -0500

    .

--- src/test/java/page/UtiliClass.java
+++ src/test/java/page/UtiliClass.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UtiliClass {
